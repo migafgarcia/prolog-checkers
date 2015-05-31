c1daa5b077b8810b0015e437fccf1e052becd9b8
migafgarcia <migafgarcia@gmail.com>
2015-05-31 12:07:18 +0100

    Added function to return the best board given a list of boards. TODO: Add function for min, Start implementing minimax tree

--- checkers.pl
+++ checkers.pl
@@ -1,4 +1,3 @@
-%asdasd
 :- use_module(library(lists)).
 
 % initial_board(?Board).
@@ -8,36 +7,46 @@
 % wq = white queen
 % 0 = white place
 % 1 = black place
+
 empty_board([
-	[0,1,0,1,0,1,0,1],
-	[1,0,1,0,1,0,1,0],
-	[0,1,0,1,0,1,0,1],
-	[1,0,1,0,1,0,1,0],
-	[0,1,0,1,0,1,0,1],
-	[1,0,1,0,1,0,1,0],
-	[0,1,0,1,0,1,0,1],
-	[1,0,1,0,1,0,1,0]]).
+	     [0,1,0,1,0,1,0,1],
+	     [1,0,1,0,1,0,1,0],
+	     [0,1,0,1,0,1,0,1],
+	     [1,0,1,0,1,0,1,0],
+	     [0,1,0,1,0,1,0,1],
+	     [1,0,1,0,1,0,1,0],
+	     [0,1,0,1,0,1,0,1],
+	     [1,0,1,0,1,0,1,0]]).
 
 initial_board([
-	[0,b,0,b,0,b,0,b],
-	[b,0,b,0,b,0,b,0],
-	[0,b,0,b,0,b,0,b],
-	[1,0,1,0,1,0,1,0],
-	[0,1,0,1,0,1,0,1],
-	[w,0,w,0,w,0,w,0],
-	[0,w,0,w,0,w,0,w],
-	[w,0,w,0,w,0,w,0]]).
+	       [0,b,0,b,0,b,0,b],
+	       [b,0,b,0,b,0,b,0],
+	       [0,b,0,b,0,b,0,b],
+	       [1,0,1,0,1,0,1,0],
+	       [0,1,0,1,0,1,0,1],
+	       [w,0,w,0,w,0,w,0],
+	       [0,w,0,w,0,w,0,w],
+	       [w,0,w,0,w,0,w,0]]).
 
 test_board_1([
-	[0,b,0,b,0,b,0,b],
-	[b,0,b,0,b,0,b,0],
-	[0,b,0,b,0,b,0,b],
-	[1,0,1,0,1,0,1,0],
-	[0,b,0,1,0,1,0,1],
-	[w,0,w,0,w,0,w,0],
-	[0,w,0,w,0,w,0,w],
-	[w,0,w,0,w,0,w,0]]).
+	      [0,b,0,b,0,b,0,b],
+	      [b,0,b,0,b,0,b,0],
+	      [0,b,0,b,0,1,0,b],
+	      [1,0,w,0,1,0,1,0],
+	      [0,b,0,b,0,1,0,1],
+	      [w,0,w,0,w,0,w,0],
+	      [0,w,0,w,0,w,0,w],
+	      [w,0,w,0,w,0,w,0]]).
 
+test_board_2([
+	      [0,b,0,b,0,b,0,b],
+	      [b,0,1,0,b,0,b,0],
+	      [0,1,0,b,0,1,0,b],
+	      [1,0,w,0,1,0,1,0],
+	      [0,b,0,b,0,1,0,1],
+	      [w,0,w,0,w,0,w,0],
+	      [0,w,0,w,0,w,0,w],
+	      [w,0,w,0,w,0,w,0]]).
 % pos(+X, +Y, +Board, -Piece).
 pos(X, Y, Board, Piece) :-
 	nth0(Y, Board, XBoard),
@@ -70,7 +79,7 @@ move(X1, Y1, X2, Y2, Board, NewBoard) :-
 	pos(X1, Y1, Empty, Place),
 	replace_in_board(X1, Y1, Place, Board, Board1),
 	replace_in_board(X2, Y2, Piece, Board1, NewBoard).
-	
+
 is_occupied(X, Y, Board) :-
 	pos(X, Y, Board, Target),
 	\+number(Target).
@@ -134,8 +143,8 @@ next_move(X, Y, Piece, Board, NewBoard) :-
 	X1 is X - 1, Y1 is Y - 1,
 	\+is_occupied(X1, Y1, Board),
 	move(X, Y, X1, Y1, Board, NewBoard).
-	
-	
+
+
 next_move(X, Y, Piece, Board, NewBoard) :-
 	Piece == w,
 	X1 is X + 1, Y1 is Y - 1,
@@ -169,8 +178,8 @@ next_move(X, Y, Piece, Board, NewBoard) :-
 	X1 is X - 1, Y1 is Y + 1,
 	\+is_occupied(X1, Y1, Board),
 	move(X, Y, X1, Y1, Board, NewBoard).
-	
-	
+
+
 next_move(X, Y, Piece, Board, NewBoard) :-
 	Piece == b,
 	X1 is X + 1, Y1 is Y + 1,
@@ -220,8 +229,8 @@ next_move(X, Y, Piece, Board, NewBoard) :-
 	X1 is X - 1, Y1 is Y - 1,
 	\+is_occupied(X1, Y1, Board),
 	move(X, Y, X1, Y1, Board, NewBoard).
-	
-	
+
+
 next_move(X, Y, Piece, Board, NewBoard) :-
 	member(Piece,[wq,bq]),
 	X1 is X + 1, Y1 is Y - 1,
@@ -233,30 +242,44 @@ next_move(X, Y, Piece, Board, NewBoard) :-
 	X1 is X - 1, Y1 is Y + 1,
 	\+is_occupied(X1, Y1, Board),
 	move(X, Y, X1, Y1, Board, NewBoard).
-	
-	
+
+
 next_move(X, Y, Piece, Board, NewBoard) :-
 	member(Piece,[wq,bq]),
 	X1 is X + 1, Y1 is Y + 1,
 	\+is_occupied(X1, Y1, Board),
 	move(X, Y, X1, Y1, Board, NewBoard).
 
+% max_board( [Board], Eval, Board) :-
+% 	evaluate_board(Eval).
+% max_board([Board|Boards], Eval, BestBoard) :-
+% 	evaluate_board(Eval),
+% 	max_board(Boards, Evals, BestBoard1),
+	
 
 
+
+% max(?Boards, ?Board).
+% Given a list of boards, returns the one with maximum evaluation
+% Aux: max_board/3, get_max_board/5
+max_boards([Board], Board).
+max_boards([Board|Boards], BestBoard) :-
+	max_boards(Boards, BestBoard1),
+	max_board(Board, BestBoard1, BestBoard).
+
+max_board(Board1, Board2, BestBoard) :-
+	evaluate_board(Board1, Eval1),
+	evaluate_board(Board2, Eval2),
+	get_max_board(Board1, Eval1, Board2, Eval2, BestBoard).
+	
+get_max_board(Board1, Eval1, Board2, Eval2, Board1) :-
+	Eval1 >= Eval2.
+get_max_board(Board1, Eval1, Board2, Eval2, Board2) :-
+	Eval1 < Eval2.
+	
 print_board([]) :- nl.
 print_board([Line|Board]) :- print_line(Line), nl, print_board(Board).
 
 print_line([]).
 print_line([Piece|Line]) :- print(Piece), print_line(Line). 
-
-% white piece
-% if not free (x-1,y-1) && (x+1,y-1)
-%  if free (x-2,y-2) && (x+2,y-2)
-%   play
-% else
-%  play
-
-% black
-% (x+1,y+1)
-% (x-1,y+1)
 
