7dd1dea80f5527fa3befe4fe670237362f08a83b
mjmoreira <mjmoreira@users.noreply.github.com>
2015-06-06 17:59:17 +0100

    Added comments to the predicates.

--- Trabalho_Damas.pl
+++ Trabalho_Damas.pl
@@ -1,5 +1,7 @@
 :- use_module(library(lists)).
 
+% board(-Board).
+% Creates a board container, with uninitialized positions
 board(game_board(A,B,C,D,E,F,G,H)):-
     functor(A,l,8), 
     functor(B,l,8),
@@ -10,6 +12,8 @@ board(game_board(A,B,C,D,E,F,G,H)):-
     functor(G,l,8), 
     functor(H,l,8).
 
+% board_initialize_empty(-Board).
+% Creates a empty board, i.e. initialized with 0 for black space and 1 for white space
 board_initialize_empty(game_board(A,B,C,D,E,F,G,H)):-
     board(game_board(A,B,C,D,E,F,G,H)),
     board_initialize_empty_even(A),
@@ -21,6 +25,8 @@ board_initialize_empty(game_board(A,B,C,D,E,F,G,H)):-
     board_initialize_empty_even(G),
     board_initialize_empty_odd(H).
 
+% board_initialize_game(-Board).
+% Creates a board with the initial state of a game, i.e. the white and the black pieces are placed.
 board_initialize_game(game_board(A,B,C,D,E,F,G,H)):-
     board(game_board(A,B,C,D,E,F,G,H)),
     board_initialize_game_even(A,b),
@@ -34,31 +40,41 @@ board_initialize_game(game_board(A,B,C,D,E,F,G,H)):-
     board_initialize_empty(Empty_Board),
     assert(empty_board(Empty_Board)).
 
+% board_initialize_empty_odd(+Line).
+% Auxiliary function that initializes a line of the board with alternating black and white spaces.
 board_initialize_empty_odd(A):-
     arg(1,A,0), arg(2,A,1),
     arg(3,A,0), arg(4,A,1),
     arg(5,A,0), arg(6,A,1),
     arg(7,A,0), arg(8,A,1).
 
+% board_initialize_empty_even(+Line).
+% Auxiliary function that initializes a line of the board with alternating black and white spaces.
 board_initialize_empty_even(A):-
     arg(1,A,1), arg(2,A,0),
     arg(3,A,1), arg(4,A,0),
     arg(5,A,1), arg(6,A,0),
     arg(7,A,1), arg(8,A,0).
 
+% board_initialize_game_odd(+Line,+Player_Symbol).
+% Auxiliary function that initializes a line of the board. The player pieces and black spaces are alternaded. 
 board_initialize_game_odd(Line,Player):-
     arg(1,Line,Player), arg(2,Line,0),
     arg(3,Line,Player), arg(4,Line,0),
     arg(5,Line,Player), arg(6,Line,0),
     arg(7,Line,Player), arg(8,Line,0).
 
+% board_initialize_game_even(+Line,+Player_Symbol).
+% Auxiliary function that initializes a line of the board. The player pieces and black spaces are alternaded.
 board_initialize_game_even(Line,Player):-
     arg(1,Line,0), arg(2,Line,Player),
     arg(3,Line,0), arg(4,Line,Player),
     arg(5,Line,0), arg(6,Line,Player),
     arg(7,Line,0), arg(8,Line,Player).
 
-    
+
+% board_print(+Board).
+% Prints the board Board to the console.
 board_print(game_board(A,B,C,D,E,F,G,H)):-
     board_print_line(A),
     board_print_line(B),
@@ -69,6 +85,8 @@ board_print(game_board(A,B,C,D,E,F,G,H)):-
     board_print_line(G),
     board_print_line(H).
 
+% board_print_line(+Line).
+% Auxiliary function that prints a line of the board.
 board_print_line(Line):-
     board_print_line_element(Line,1),
     board_print_line_element(Line,2),
@@ -80,6 +98,8 @@ board_print_line(Line):-
     board_print_line_element(Line,8),
     nl.
 
+% board_print_line_element(+Line,+Index).
+% Auxiliary function that prints a element of the board to the console.
 board_print_line_element(Line,Index):-
     arg(Index,Line,E),
     print(E),
@@ -88,15 +108,22 @@ board_print_line_element(Line,Index):-
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-
-pos(X,Y,Board,E):-
+% pos(+Board,+X,+Y,-Element).
+% Returns the value of the position (X,Y). It can be the board color or the piece at that location.
+pos(Board, X, Y, E):-
     arg(Y,Board,T),
     arg(X,T,E).
 
+% replace(+Board,+X,+Y,+Element,-New_Board).
+% Places or replaces the element at position (X,Y) with Element and returns a New_Board. 
 replace(Board, X, Y, Element, New_Board):-
     functor(New_Board,game_board,8),
     replace_in_board(Board,X,Y,Element,New_Board,1).
 
+% replace_in_board(+Board,+X,+Y,+Element,+New_Board,+Iterator).
+% Auxiliary Function
+% Receives an uninitialized New_Board and replaces element at position (X,Y) from Board
+% with Element in the New_Board. All the other positions are copies of Board.
 replace_in_board(_,_,_,_,_,Iterator):- Iterator > 8, !.
 replace_in_board(Board, X, Y, Element, New_Board, Iterator):-
     Iterator == Y, !,
@@ -112,6 +139,9 @@ replace_in_board(Board, X, Y, Element, New_Board, Iterator):-
     Iterator_Next is Iterator + 1,
     replace_in_board(Board, X, Y, Element, New_Board, Iterator_Next).
 
+% replace_in_line(+Line,+X,+Element,+New_Line,+Iterator).
+% Auxiliary Funciton
+% Replaces the element at index X with Element. All other positions are copies from Line.
 replace_in_line(_,_,_,_, Iterator):- Iterator > 8, !.
 replace_in_line(Line, X, Element, New_Line, Iterator):-
     Iterator == X, !,
